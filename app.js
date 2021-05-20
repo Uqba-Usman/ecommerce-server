9bf96bd27e74cd0110d3191cd1d1795601e43935
muqba <uqbamehar312@gmail.com>
2021-05-20 23:01:55 +0500

    page

--- app.js
+++ app.js
@@ -14,8 +14,8 @@ var usersRouter = require("./routes/users");
 var app = express();
 
 // view engine setup
-// app.set("views", path.join(__dirname, "views"));
-// app.set("view engine", "pug");
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
 var corsOptions = {
   // origin: ["https://minicab-client.herokuapp.com/", "http://localhost:3000/"],
   origin: "*",
